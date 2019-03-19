1afbb8ae714641c0ff04881d99820cb6b5bc8df1
Waldemar Bira <waldek.bira@gmail.com>
2019-03-19 09:02:34 +0100

    Implemented test mutation

--- client/client_test.go
+++ client/client_test.go
@@ -20,24 +20,51 @@ type response struct {
 }
 
 func TestGraphQlQuery(t *testing.T) {
-	client := graphql.NewClient("http://localhost:8080/query")
+	query := `
+	query findTodos {
+	  	todos {
+	      text
+	      done
+	      user {
+	        name
+	      }
+	    }
+	}
+	`
 
-	request := graphql.NewRequest(`
-query findTodos {
-  	todos {
-      text
-      done
-      user {
-        name
-      }
-    }
-}
-	`)
-
-	ctx := context.Background()
-	var response response
-	if err := client.Run(ctx, request, &response); err != nil {
+	response, err := makeGraphQlRequest(query)
+	if err != nil {
 		t.Errorf("%v", err)
 	}
 	fmt.Printf("Request %v", response)
+}
+
+func TestGraphQlMutation(t *testing.T) {
+	query := `
+mutation createTodo {
+  createTodo(input:{text:"Newtodo", userId:"3"}) {
+    user {
+      id
+    }
+    done
+    text
+  }
+}`
+
+	response, err := makeGraphQlRequest(query)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	fmt.Printf("Request %v", response)
+}
+
+func makeGraphQlRequest(query string) (*response, error) {
+	client := graphql.NewClient("http://localhost:8080/query")
+	request := graphql.NewRequest(query)
+	ctx := context.Background()
+	var response response
+	if err := client.Run(ctx, request, &response); err != nil {
+		return nil, err
+	}
+	return &response, nil
 }
